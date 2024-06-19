e89f13a19f94fa1ed2a6bdb51b5ba4df2a1dd5eb
AhmedHamed3699 <Ahmed.Hamed03@eng-st.cu.edu.eg>
2024-06-20 00:13:18 +0300

    done

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Spelling mistake fixing
+
+exec { 'spelling-mistake-fix':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,1 @@
+This project is for fixing web stack bugs
